fccdfaf29787c002dbc111a154e8e86bc89e76d8
seong cho <mileage77@hotmail.com>
2022-10-21 19:27:54 -0500

    second commit

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-//랜덤번호 지정
-//유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
-//만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
-//랜덤번호가 유저번호보다 작으면 <Down!!!>
-//랜덤번호가 유저번호보다 크면 <Up!!>
-//reset 버튼을 누르면 게임이 리셋된다.
-//5번의 기회를 다쓰면 게임이 끝난다. (더이상 추측 불가능, 버튼이 disable)
-//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
-//유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다.
-
-
 let computerNum = 0
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
@@ -26,15 +15,15 @@ userInput.addEventListener("focus", function (){
 });
 
 function pickRandomNum (){
-    computerNum = Math.floor(Math.random()*100)+1;
+    computerNum = Math.floor(Math.random()*30)+1;
     console.log("정답:", computerNum);
 }
 
 function play(){
     let userValue = userInput.value;
 
-    if(userValue < 1 || userValue > 100){
-        resultArea. textContent="1과 100사이 숫자를 입력해 주세요"
+    if(userValue < 1 || userValue > 30){
+        resultArea. textContent="1과 30사이 숫자를 입력해 주세요"
         return;
     }
     if(history.includes(userValue)){
@@ -75,3 +64,7 @@ function reset(){
 }
 
 pickRandomNum();
+
+
+
+
